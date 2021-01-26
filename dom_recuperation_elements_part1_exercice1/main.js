c3b16a9dbd7719d2c39141630045dd0a0e34c9ba
Sébastien <seb.lasante@hotmail.com>
2021-01-26 10:02:56 +0100

    fini

---
+++ dom_recuperation_elements_part1_exercice1/main.js
@@ -0,0 +1,13 @@
+let h1 = document.getElementsByTagName ("h1");
+console.log(h1);
+
+let h3 = document.getElementsByTagName('h3')
+console.log(h3 [0]);
+console.log(h3 [1]);
+console.log(h3 [2]);
+
+let p = document.getElementsByTagName('p')
+console.log(p);
+
+let li = document.getElementsByTagName('li')
+console.log(li [2]);
